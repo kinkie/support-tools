762d8be285e92fc1a1ef34193340831bdd7006a7
Francesco Chemolli <kinkie@squid-cache.org>
2012-09-18 14:45:43 +0200

    transformed remove-duplicate-empty-lines.pl into self-recursive

--- remove-duplicate-empty-lines.pl
+++ remove-duplicate-empty-lines.pl
@@ -7,26 +7,84 @@
 # and COPYRIGHT.
 #
 # 
-# USAGE: remove-duplicate-empty-lines.pl filename.h >filename.h.adjusted
+# USAGE: remove-duplicate-empty-lines.pl file|dir [file|dir] ...
 #
 # Aim of this script is to reduce sets of duplicate empty lines
 # onto a single empty line.
 #
-# Suggested usage:
-# for file in $(find . -name \*.h -or -name \*.cc -or -name \*.cci); do echo $file; /full/path/to/remove-duplicate-empty-lines.pl $file >$file.adj; mv $file.adj $file; done
+# will recurse into subdirectories and only handle files with 
+#  .c, .h or .cci extensions
 
 use strict;
 use warnings;
-my $el=0; #1 if last line was empty
 
-while (<>) {
-  if (m!^\s*$!) {
-    unless ($el) {
-      print;
+#argument: filename
+#  will check for duplicate empty lines in filename and remove them.
+#  Will modify the file in-place.
+sub handle_file {
+  my $fh;
+  my @out;
+  my $filename=shift @_;
+  my $el=0; #1 if last line was empty
+  my $touched=0;
+
+  print STDERR "$filename...";
+  open ($fh,$filename) || die "can't open file $filename";
+
+  while (<$fh>) {
+    if (m!^\s*$!) {
+      if ($el) {
+        #skip a line
+        $touched=1;
+      } else {
+        #first empty line, don't skip
+        push @out,$_;
+      }
+      $el=1;
+    } else {
+      push @out,$_;
+      $el=0;
     }
-    $el=1;
+  }
+  close $fh;
+
+  if ($touched) {
+    open $fh,">$filename" || die "can't open file $filename for writing";
+    print $fh @out;
+    close $fh;
+    print STDERR " Modified\n";
   } else {
-    print;
-    $el=0;
+    print STDERR "\r";
+  }
+}
+
+#argument: dirname
+# will call handle_file for all files in all subdirectories of the argument
+# that have a .cc, .cci or .h suffix and don't begin with a dot.
+sub handle_dir {
+  my $dh;
+  my $dirname=shift @_;
+  opendir($dh,$dirname) || die "can't open dir $dirname";
+  my @dir=readdir($dh);
+  closedir($dh);
+  my @files=grep { -f "$dirname/$_" && !/^\./ && /\.(h|cci?)$/ } @dir;
+  my @dirs=grep { -d "$dirname/$_" && !/^\./ } @dir;
+
+  foreach (@files) {
+    &handle_file("$dirname/$_");
+  }
+  foreach (@dirs) {
+    &handle_dir("$dirname/$_");
+  }
+}
+
+foreach (@ARGV) {
+  if (-d $_) {
+    &handle_dir($_);
+    next;
+  }
+  if (-f $_) {
+    &handle_file($_);
+    next;
   }
 }
