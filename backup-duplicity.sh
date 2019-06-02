6279bcd7bcf2c9955cfb0c61e2da89cb8982993c
Francesco Chemolli <kinkie@squid-cache.org>
2019-06-02 09:59:13 +0100

    Add duplicity-based backup

---
+++ backup-duplicity.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -lt 2 ] ; then
+    echo "$0 full|incr destination"
+    exit 0
+fi
+
+case $1 in
+	full|incr) : ;;
+	*) echo "$0 full|incr"; exit 1;;
+esac
+
+duplicity $1 \ 
+    --volsize 200 \
+    --exclude /proc --exclude /sys \
+    --exclude /var/log \
+    --exclude-regexp '.*(btlayer-).*' \
+    --exclude-if-present CACHEDIR.TAG \
+    --no-encryption --progress --ssh-askpass \
+    / sftp://$2/$(hostname -s)
